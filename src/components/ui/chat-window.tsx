4f45cab54669871e5cf305db382596bc6486eacc
smart419-expert <darkdev0419@gmail.com>
2025-09-07 08:14:40 +0200

    fixed chat height

--- src/components/ui/chat-window.tsx
+++ src/components/ui/chat-window.tsx
@@ -258,7 +258,7 @@ export function ChatWindow({ clientId, clientName, className }: ChatWindowProps)
         </CardHeader>
 
         <CardContent className="p-0 flex-1 overflow-hidden">
-          <div ref={chatContainerRef} className="h-full">
+          <div ref={chatContainerRef} className="h-auto">
             {settings.useMockChat ? (
               <MockChat
                 clientId={clientId}
